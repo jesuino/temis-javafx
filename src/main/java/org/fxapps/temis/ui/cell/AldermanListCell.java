7a1d1ec443f1403519649c6fc708d65d4bf0d362
William Antonio <william.fatecsjc@gmail.com>
2016-09-08 14:20:28 -0300

    Change selected alderman's font

--- src/main/java/org/fxapps/temis/ui/cell/AldermanListCell.java
+++ src/main/java/org/fxapps/temis/ui/cell/AldermanListCell.java
@@ -8,6 +8,8 @@ import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.ListCell;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 
 public class AldermanListCell extends ListCell<Alderman> {
 
@@ -37,6 +39,20 @@ public class AldermanListCell extends ListCell<Alderman> {
 		imageView.setFitHeight(150);
 		imageView.setStyle("-fx-effect: dropshadow(three-pass-box, rgba(0,0,255,0.8), 30, 0, 0, 0);");
 		return imageView;
+	}
+
+	@Override
+	public void updateSelected(boolean selected) {
+		super.updateSelected(selected);
+		if (selected) {
+			this.getGraphic().setScaleX(1.1);
+			this.getGraphic().setScaleY(1.1);
+			this.setFont(Font.font(Font.getDefault().getFamily(), FontWeight.BOLD, Font.getDefault().getSize() + 1));
+		} else {
+			this.getGraphic().setScaleX(1);
+			this.getGraphic().setScaleY(1);
+			this.setFont(Font.getDefault());
+		}
 	}
 
 }
